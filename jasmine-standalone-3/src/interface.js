f5d0c7f2ddcc9ffda5a3a3f2a565999b391807ed
tracht <marija.trachtenberg@gmail.com>
2020-05-26 22:16:01 +0100

    add Austin, Toronto, and Paris to cities list; add weather API for cloudyness

--- jasmine-standalone-3/src/interface.js
+++ jasmine-standalone-3/src/interface.js
@@ -64,4 +64,11 @@ $(document).ready(function() {
     })
   });
 
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#cloudy-meter').text(data.clouds.all)
+    })
+  });
+
 });
