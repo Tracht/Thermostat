00ef5ecd4d7bfa986de79c0ab37e1c11cbe191d5
tracht <marija.trachtenberg@gmail.com>
2020-05-26 21:38:30 +0100

    add weather API for berlin, tokyo, london

--- jasmine-standalone-3/src/interface.js
+++ jasmine-standalone-3/src/interface.js
@@ -36,4 +36,11 @@ $(document).ready(function() {
     $('#_currentEnergyUsage').text(thermostat._currentEnergyUsage());
   };
 
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp)
+    })
+  });
+
 });
